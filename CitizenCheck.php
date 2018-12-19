9281ee93b2089934e0a57c478af2ac1018d9c584
Siwat Techavoranant <keen@keendev.net>
2018-12-19 17:22:41 +0700

    Use SOAP 1.2

--- CitizenCheck.php
+++ CitizenCheck.php
@@ -18,7 +18,7 @@ class CitizenCheck {
      *
      * @param string $pid Citizen ID (13 digits)
      * @param string $dob Date of Birth e.g. 2018-12-31 or 25611231
-     * @param bool   $strictSSL Enable SSL server validation
+     * @param bool $strictSSL Enable SSL server validation
      * @return bool
      * @throws Exception
      */
@@ -28,33 +28,44 @@ class CitizenCheck {
             $birthTimestamp = strtotime($dob);
             $dob = (date('Y', $birthTimestamp) + 543) . date('md', $birthTimestamp);
         }
-        
+
+        $postContent = '<?xml version="1.0" encoding="utf-8"?>
+<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+  <soap12:Body>
+    <CheckDeathStatus xmlns="http://tempuri.org/">
+      <pid>'.$pid.'</pid>
+      <dob>'.$dob.'</dob>
+    </CheckDeathStatus>
+  </soap12:Body>
+</soap12:Envelope>';
+
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://idcard.bora.dopa.go.th/CheckStatus/POPStatusService.asmx/CheckDeathStatus");
+        curl_setopt($ch, CURLOPT_URL, "https://idcard.bora.dopa.go.th/CheckStatus/POPStatusService.asmx");
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('pid' => $pid, 'dob' => $dob)));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postContent);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/soap+xml; charset=utf-8']);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)');
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, $strictSSL);
         $output = curl_exec($ch);
         curl_close($ch);
-        
-        if (strpos($output, 'POPOut') === false) {
+
+        if (strpos($output, '<CheckDeathStatusResult>') === false) {
             throw new Exception('Unexpected message from API provider');
         } elseif (self::getStringBetween($output, '<isError>', '</isError>') === 'true') {
             // <isError>true</isError><errorDesc>ข้อมูลไม่ถูกต้อง</errorDesc>
             // Person doesn't exist
             return false;
-        } elseif (self::getStringBetween($output, '<stCode>', '</stCode>') === '1') {
+        } elseif (self::getStringBetween($output, '<stCode>', '</stCode>') === '0') {
+            // <stCode>0</stCode><stDesc>สถานะปกติ (มีชีวิต)</stDesc>
+            return true;
+        } else {
             // <stCode>1</stCode><stDesc>สถานะเสียชีวิต</stDesc>
             // Dead
             return false;
-        } else {
-            // <stCode>0</stCode><stDesc>สถานะปกติ (มีชีวิต)</stDesc>
-            return true;
         }
     }
-    
+
     protected static function getStringBetween(string $haystack, string $start, string $end) {
         $haystack = ' ' . $haystack;
         $ini = strpos($haystack, $start);
@@ -63,8 +74,8 @@ class CitizenCheck {
         }
         $ini += strlen($start);
         $len = strpos($haystack, $end, $ini) - $ini;
-        
+
         return substr($haystack, $ini, $len);
     }
-    
+
 }
